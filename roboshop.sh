e84119f9c256b23c3b62a214c6645e244a184fa0
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-01 20:40:34 -0800

    instances

---
+++ roboshop.sh
@@ -0,0 +1,16 @@
+
+
+
+SG-ID="sg-0643d1da123417a9c"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do 
+    aws ec2 run-instances \
+  --image-id $AMI_ID \
+  --instance-type t3.micro \
+  --security-group-ids $SG-ID \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"\
+  --query 'ResourceType[0].Instances[0].PrivateIpAddress' --output text
+   
+done
