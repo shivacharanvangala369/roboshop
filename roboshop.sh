c2eeba317bb31b9bbf9b828a1279b9689f523cfb
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-01 20:43:16 -0800

    instnaces

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-SG-ID="sg-0643d1da123417a9c"
+SG_ID="sg-0643d1da123417a9c"
 AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
@@ -9,7 +9,7 @@ do
   aws ec2 run-instances \
   --image-id $AMI_ID \
   --instance-type t3.micro \
-  --security-group-ids $SG-ID \
+  --security-group-ids $SG_ID \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"\
   --query 'ResourceType[0].Instances[0].PrivateIpAddress' --output text
    
