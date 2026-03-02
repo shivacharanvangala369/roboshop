694652cd3b39f45cf8cd0498a7323ea3449c2094
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-01 20:47:31 -0800

    ins

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,8 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do 
-  aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"
-  --query 'ResourceType[0].Instances[0].PrivateIpAddress' --output text
+  aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --tag-specifications 
+  "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"--query 'ResourceType[0].Instances[0].
+  PrivateIpAddress' --output text
    
 done
