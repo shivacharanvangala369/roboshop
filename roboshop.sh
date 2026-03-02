26c13bea473fb5abbfa3113e99d5e36737504685
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-01 20:42:35 -0800

    instances

--- roboshop.sh
+++ roboshop.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 
 SG-ID="sg-0643d1da123417a9c"
@@ -6,7 +6,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do 
-    aws ec2 run-instances \
+  aws ec2 run-instances \
   --image-id $AMI_ID \
   --instance-type t3.micro \
   --security-group-ids $SG-ID \
