35afb397a4304b9100e78aabe396b0b209cf83dd
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-02 21:03:28 -0800

    all_instances

---
+++ payment.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/roboshop"
+LOG_FILE="$LOG_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.devcops.online
+
+if [ $USERID -ne 0 ]; then
+     echo -e "$R please run this script using root user" | tee -a $LOG_FILE
+     exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e " $2... $R FAILURE" | tee -a $LOG_FILE
+    else
+        echo -e "$2... $G SUCCESS" | tee -a $LOG_FILE
+    fi
+}
+
+
+
+dnf install python3 gcc python3-devel -y  &>>$LOG_FILE
+VALIDATE  $? "install python3"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOG_FILE
+VALIDATE  $? "craeteing user"
+
+rm -rf /app/*   &>>$LOG_FILE
+VALIDATE  $? "removeing app dir in /"
+
+mkdir -p /app 
+
+
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip   &>>$LOG_FILE
+cd /app 
+unzip /tmp/payment.zip  &>>$LOG_FILE
+VALIDATE  $? "downloading and zip the code"
+
+
+cd /app   &>>$LOG_FILE
+pip3 install -r requirements.txt  &>>$LOG_FILE
+VALIDATE  $? "pip3 install"
+
+
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
+VALIDATE  $? "copying service files"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE  $? "daemon-reload"
+
+systemctl enable payment   &>>$LOG_FILE
+systemctl start payment  &>>$LOG_FILE
+VALIDATE  $? "enable and start mysql"
