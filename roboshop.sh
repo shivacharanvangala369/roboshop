77796d42f626259022114c55cb2374d3e33ed43d
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-02 16:41:47 -0800

    all_instances

--- roboshop.sh
+++ roboshop.sh
@@ -10,9 +10,10 @@ for instance in $@
 do 
   INSTANCE_ID=$( aws ec2 run-instances \
   --image-id $AMI_ID \
-  --instance-type t3.micro \
+  --instance-type "t3.micro" \
+  --security-group-ids $SG_ID \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-  --query 'Instances[0].IntanceId[0]' \
+  --query 'Instances[0].IntanceId' \
   --output text )
    
   if [ $instance == "frontend" ]; then
