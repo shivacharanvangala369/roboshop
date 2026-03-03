655a97d64a32b7854ac82b8b6c4a1bf6fa67a5b8
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-02 20:16:47 -0800

    all_instances

---
+++ redis.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/roboshop"
+LOG_FILE="$LOG_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.devcops.online
+
+if [ $USERID -ne 0 ]; then
+     echo -e "$R please run this script using root user" | tee -a $LOG_FILE
+     exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e " $2... $R FAILURE" | tee -a $LOG_FILE
+    else
+        echo -e "$2... $G SUCCESS" | tee -a $LOG_FILE
+    fi
+}
+
+
+
+dnf module disable redis -y    &>>$LOG_FILE
+dnf module enable redis:7 -y
+dnf install redis -y 
+VALIDATE  $? " enable and install redis"
+
+
+sed -i -e "s/127.0.0.1/0.0.0.0/g" -e "/protected-mode/ c protected-mode no" /etc/redis/redis.conf 
+VALIDATE  $? "Allowing remote connection"
+
+systemctl enable redis   &>>$LOG_FILE
+systemctl start redis    &>>$LOG_FILE
+VALIDATE  $? "enable and start redis"
