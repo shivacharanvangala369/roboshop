e30bb47c428cfdb61d02ca73290892015a8541db
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-02 16:43:44 -0800

    all_instances

--- roboshop.sh
+++ roboshop.sh
@@ -8,58 +8,58 @@ DOMAIN_NAME="devcops.online"
 
 for instance in $@
 do 
-  INSTANCE_ID=$( aws ec2 run-instances \
-  --image-id $AMI_ID \
-  --instance-type "t3.micro" \
-  --security-group-ids $SG_ID \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-  --query 'Instances[0].IntanceId' \
-  --output text )
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].IntanceId' \
+    --output text )
    
-  if [ $instance == "frontend" ]; then
-    IP=$(
-      aws ec2 describe-instance \
-      --instance-ids $INSTANCE_ID \
-      --query 'Reservations[].Instances[].PublicIpAddress' \
-      --output text )
+    if [ $instance == "frontend" ]; then
+      IP=$(
+        aws ec2 describe-instance \
+        --instance-ids $INSTANCE_ID \
+        --query 'Reservations[].Instances[].PublicIpAddress' \
+        --output text )
 
-    RECORD_NAME="$DOMAIN_NAME"
-  else
-    IP=$(
-      aws ec2 describe-instance \
-      --instance-ids $INSTANCE_ID \
-      --query 'Reservations[].Instances[].PrivateIpAddress' \
-      --output text )
+      RECORD_NAME="$DOMAIN_NAME"
+    else
+      IP=$(
+        aws ec2 describe-instance \
+        --instance-ids $INSTANCE_ID \
+        --query 'Reservations[].Instances[].PrivateIpAddress' \
+        --output text )
 
-    RECORD_NAME="$instance.$DOMAIN_NAME"
+      RECORD_NAME="$instance.$DOMAIN_NAME"
+        
+      fi
       
-    fi
-    
-    echo "IP Address: $IP"
+      echo "IP Address: $IP"
 
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
+      aws route53 change-resource-record-sets \
+      --hosted-zone-id $ZONE_ID \
+      --change-batch '
 
-    {
-      "Comment": "Updating record",
-      "Changes": [
-        {
-          "Action": "UPSERT",
-          "ResourceRecordSet": {
-            "Name": "'$RECORD_NAME'",
-            "Type": "A",
-            "TTL": 1,
-            "ResourceRecords": [
-              {
-                "Value": "'$IP'"
-              }
-            ]
+      {
+        "Comment": "Updating record",
+        "Changes": [
+          {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+              "Name": "'$RECORD_NAME'",
+              "Type": "A",
+              "TTL": 1,
+              "ResourceRecords": [
+                {
+                  "Value": "'$IP'"
+                }
+              ]
+            }
           }
-        }
-      ]
-    }
-    '
+        ]
+      }
+      '
 
 echo "record updated for $instance"
 
