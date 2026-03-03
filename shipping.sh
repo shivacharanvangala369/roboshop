dcb32488c2964e46bca2876f9f27e46de1df5eee
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-02 21:39:38 -0800

    all_instances

--- shipping.sh
+++ shipping.sh
@@ -82,15 +82,16 @@ dnf install mysql -y      &>>$LOG_FILE
 
 
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+if [ $? -ne 0 ]; then
 
-
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql 
-
-
-
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
-
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+    VALIDATE $? "Loaded data into MySQL"
+else
+    echo -e "data is already loaded ... $Y SKIPPING $N"
+fi
 
 
 systemctl restart shipping     &>>$LOG_FILE
