f90f4dac242595ff41da90da3472602cc3cc2d1e
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-02 21:31:54 -0800

    all_instances

--- frontend.sh
+++ frontend.sh
@@ -46,7 +46,7 @@ VALIDATE  $? "unzipping frontend"
 
 rm -rf /etc/nginx/nginx.conf
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf      &>>$LOG_FILE
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf      
 VALIDATE  $? "copying conf file"
 
 
