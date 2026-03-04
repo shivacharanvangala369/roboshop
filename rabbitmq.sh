07718c1e88a54444e2929e8e490a95726576316d
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-03 21:24:48 -0800

    all_shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -25,16 +25,18 @@ VALIDATE(){
 }
 
 
-cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo   &>>$LOG_FILE
+VALIDATE  $? "copying service files"
+
+dnf install rabbitmq-server -y  &>>$LOG_FILE
+VALIDATE  $? "install rabbitmq"
 
 
-dnf install rabbitmq-server -y
+systemctl enable rabbitmq-server &>>$LOG_FILE
+systemctl start rabbitmq-server &>>$LOG_FILE
+VALIDATE  $? "enable and start server"
 
 
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
-
-
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-
+rabbitmqctl add_user roboshop roboshop123    &>>$LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"     &>>$LOG_FILE
+VALIDATE  $? "add and set permissions"
